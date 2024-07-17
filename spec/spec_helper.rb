6a3340f50ce68e1aa3667fd15863ca266f00bfcf
Eugenio Garcia <eugenio5039@gmail.com>
2024-07-17 11:10:30 -0600

    Add - file fixtures support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require "redesigned/waddle"
+require_relative "support/file_fixtures"
 
 RSpec.configure do |config|
+  config.include FileFixtures
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
